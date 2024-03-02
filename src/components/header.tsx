d1d0e216ed29e81a1386246c659d7033c7d66cc3
Hideaki Gabriel <hideaki.gabriel@gmail.com>
2024-03-02 15:51:27 -0300

    feat: add Header component

---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return ( 
+    <header>
+      <div className="flex items-center justify-between px-2 font-medium">
+        <p>Welcome to my portfolio</p>
+        <p>Front End Developer | UI Designer</p>
+      </div>
+    </header>
+   );
+}
+ 
+export default Header;
