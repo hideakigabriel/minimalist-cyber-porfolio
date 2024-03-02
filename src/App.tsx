681da4200a8213d74d72a62dafb22f5dfe8a9937
Hideaki Gabriel <hideaki.gabriel@gmail.com>
2024-03-02 15:50:55 -0300

    chore: setup initial

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Header from './components/header'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Header />
+        <h1>Hello World!!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
